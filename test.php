660dcf79f297a25118b2833d78bcb10ade37279a
jessica <jagg1996.jj@gmail.com>
2017-07-07 14:06:07 -0500

    actau

--- test.php
+++ test.php
@@ -1,8 +1,15 @@
 <?php
- requise 'index.php';
-class travis extends PHPUnit_Framework_TestCase{
-function suma($a,$b) {
-      $this->assertEquals(2+2,4);
-   }
-
+require 'index.php';
+class CalculatorTests extends PHPUnit_Framework_TestCase
+{
+private $calculator;
+protected function setUp()
+{
+$this->index= new index();
+}
+public function testAdd()
+{
+$result=$this->index->add(1,2);
+$this->assertEquals(3,$result);
+}
 }
